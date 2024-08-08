9e74e22ed7b9b3fe48d98b196f6024c8cda15274
Aswin <aswinash19@gmail.com>
2024-08-08 12:32:30 +0530

    Prisma Setup

---
+++ server/index.ts
@@ -0,0 +1,17 @@
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const data = await prisma.user.create({
+    data: {
+      name: 'Rich',
+      email: 'hello@prisma.com',
+      department: 'cse',
+      section: '5a',
+    },
+  });
+
+  console.log('Data',data)
+}
+
